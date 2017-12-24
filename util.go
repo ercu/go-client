7895b50ef1d37df889fb313a5117b779db40bd93
Ercüment Eser <ercument.eser@cardtek.com>
2017-12-25 01:53:22 +0300

    add support for string array fields (multi-valued) while converting interface to url.Values

--- util.go
+++ util.go
@@ -1,42 +1,43 @@
 package client
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/url"
+	"reflect"
 	"strconv"
 	"strings"
 )
 
 // ToValues converts a Content type to a url.Values to use with a Ponzu Go client
 func ToValues(p interface{}) (url.Values, error) {
-	// encode p to JSON
-	j, err := json.Marshal(p)
-	if err != nil {
-		return nil, err
-	}
-
-	// decode json to url.Values
-	var kv map[string]interface{}
-	err = json.Unmarshal(j, &kv)
-	if err != nil {
-		return nil, err
-	}
-
 	vals := make(url.Values)
-	for k, v := range kv {
-		switch v.(type) {
-		case []string:
-			s := v.([]string)
-			for i := range s {
-				if i == 0 {
-					vals.Set(k, s[i])
-				}
 
-				vals.Add(k, s[i])
+	t := reflect.TypeOf(p)
+	v := reflect.Indirect(reflect.ValueOf(p))
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldValue := v.Field(i)
+		field := t.Field(i)
+		jsonTag := field.Tag.Get("json")
+
+		if jsonTag == "" {
+			continue
+		}
+
+		switch fieldValue.Kind() {
+		case reflect.Slice:
+			for j := 0; j < fieldValue.Len(); j++ {
+
+				if j == 0 {
+					vals.Set(jsonTag, fmt.Sprintf("%v", fieldValue.Index(j)))
+				} else {
+
+					vals.Add(jsonTag, fmt.Sprintf("%v", fieldValue.Index(j)))
+				}
 			}
 		default:
-			vals.Set(k, fmt.Sprintf("%v", v))
+			vals.Set(jsonTag, fmt.Sprintf("%v", fieldValue))
+
 		}
 	}
 
